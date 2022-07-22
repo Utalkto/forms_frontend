5b974b4f478020f34a43b9707ad803e81e0678ad
i27ae15 <andresruse18@gmail.com>
2022-07-22 16:48:51 -0400

    can add more fields to the form, can tell if it is reuired, can enable score to a field, can change its name'

--- src/components/cards/FormCreationCard.component.jsx
+++ src/components/cards/FormCreationCard.component.jsx
@@ -1,3 +1,4 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { Component } from "react";
 
 
@@ -15,42 +16,111 @@ class FormCreationCard extends Component {
                         
                         <h4 className="card-title" 
                         style={{fontWeight: "700"}}
-                        onClick={() => this.props.functions.openRightbar(0)}
+                        onClick={() => this.props.functions.openRightbar(0, false)}
                         >
                             {this.props.title}
                         </h4>
                         <p className="card-text"
-                        onClick={() => this.props.functions.openRightbar(1)}
+                        onClick={() => this.props.functions.openRightbar(1, false)}
                         >
                             {this.props.textBody}
                         </p>
                     </div>
                 <div>
 
-                    
-                    {
-                        this.props.elements.map((element, index) => {
+                    <div style={{padding: '10px 25px'}}>
 
-                            if (element.name !== 'Form Title' && element.name !== 'Form Body'){
-                                return(
-                                    <div class="form-group">
-                                        <label for="basicInput">{element.label}</label>
-                                        <input 
-                                        type={element.type} 
-                                        class="form-control" 
-                                        name={element.name} 
-                                        id="basicInput"
-                                        placeholder={element.placeholder}/>
+                        {
+                            this.props.elements.map((element, index) => {
+
+
+                                if (element.name !== 'Form Title' && element.name !== 'Form Body' && !Array.isArray(element)){
+                                    
+                                    if (element.type !== 'select'){
+                                        return(
+                                            <div class="form-group">
+                                                <label htmlFor="basicInput">{element.label} {element.required && '*' } </label>
+                                                <input 
+                                                type={element.type} 
+                                                className="form-control" 
+                                                name={element.name} 
+                                                id="basicInput"
+                                                placeholder={element.placeholder}
+                                                onClick={() => this.props.functions.openRightbar(index, false)}
+                                                />
+                                            </div>
+                                        );
+                                    }
+                                    else if (element.type === 'select'){
+
+                                        return(
+                                            <div class="form-group">
+
+                                                <label htmlFor="basicInput">{element.label} {element.required && '*' } </label>
+
+                                                <select class="form-select" id="basicSelect"
+                                                onClick={() => this.props.functions.openRightbar(index, false)}
+                                                
+                                                >
+
+                                                    {
+                                                    
+                                                    element.options.map((option, index) => {
+                                                        return <option>{option.value}</option>
+                                                    })
+                                                    
+                                                    }
+                                                </select>
+
+                                            </div>
+                                        );
+
+                                    
+                                    }
+
+                                } else if (Array.isArray(element)) {
+
+
+                                    return (
+                                    
+                                    <div className="row">
+                                        <div class="col-md-6">
+
+                                            <div class="form-group">
+                                                <label htmlFor="basicInput">{element[0].label} {element[0].required && '*' }</label>
+                                                <input 
+                                                type={element[0].type} 
+                                                className="form-control" 
+                                                name={element[0].name} 
+                                                id="basicInput"
+                                                placeholder={element[0].placeholder}
+                                                onClick={() => this.props.functions.openRightbar(index, 0)}
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                        
+                                            <div class="form-group">
+                                                <label htmlFor="basicInput">{element[1].label} {element[1].required && '*' }</label>
+                                                <input 
+                                                type={element[1].type} 
+                                                className="form-control" 
+                                                name={element[1].name} 
+                                                id="basicInput"
+                                                placeholder={element[1].placeholder}
+                                                onClick={() => this.props.functions.openRightbar(index, 1)}
+                                                />
+                                            </div>
+                                        </div>
                                     </div>
-                                )
-                            }
-                        })
+                                    );
+                                }
+                            })
+                        }   
 
+                    </div>
 
-
-
-
-                    }   
                 </div>
                 <div className="card-footer d-flex justify-content-end" >
                     <button className="btn btn-primary block">{this.props.buttonText}</button>
--- src/components/sideBar/Rightbar.component.jsx
+++ src/components/sideBar/Rightbar.component.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 
+import './Rightbar.styles.css';
+
 
 class Rightbar extends Component {
 
@@ -7,8 +9,9 @@ class Rightbar extends Component {
 
     }
 
-
     render () {
+
+        const { firstIndex, secondIndex } = this.props;
 
         return (
 
@@ -17,11 +20,12 @@ class Rightbar extends Component {
                     <div className="sidebar-header">
                         <div className="d-flex justify-content-between">
                             <div className="logo">
-                                <h3 style={{fontWeight: "700"}}>{this.props.element.name}</h3>
+                                <h3 style={{fontWeight: "700"}}>{this.props.element.inputTypeName}</h3>
                                 {/* <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcSet="" /></a> */}
                             </div>
-                            <button onClick={this.props.functions.closeRightBar}>
-                                Close 
+                            <button 
+                            className="btn btn-primary"
+                            onClick={this.props.functions.closeRightBar}>Close 
                             </button>
                             <div className="toggler">
                                 <a href="#" className="sidebar-hide d-xl-none d-block"><i className="bi bi-x bi-middle"></i></a>
@@ -30,21 +34,126 @@ class Rightbar extends Component {
                     </div>
                     <div className="sidebar-menu">
                         <ul className="menu">
-                            <li className="sidebar-title">Text</li>
-                            <li className="sidebar-item">
-                                <input type="text" className="form-control" 
-                                value={this.props.element.value}
-                                onChange={(e) => this.props.functions.updateComponentValue(e, this.props.componentIndex)}
-                                
-                                />
-                            </li>
 
-                            {/* <li className="sidebar-item active ">
-                                <a href="index.html" className='sidebar-link'>
-                                    <i className="bi bi-grid-fill"></i>
-                                    <span>Dashboard</span>
-                                </a>
-                            </li> */}
+                            {
+                                this.props.element.element_type === 0 ? 
+                                <div>
+                                    <li className="sidebar-title sidebar-title-right">Text</li>
+                                    <li className="sidebar-item">
+                                        <input type="text" className="form-control" 
+                                        value={this.props.element.value}
+                                        onChange={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'value')}
+                                        />
+                                    </li>
+                                </div>
+
+                                :
+                                
+                                <div>
+                                    <li className="sidebar-title sidebar-title-right">Label</li>
+                                    <li className="sidebar-item">
+                                        <input type="text" className="form-control" 
+                                        value={this.props.element.label}
+                                        onChange={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'label')}
+                                        />
+                                    </li>
+
+                                    <li className="sidebar-title sidebar-title-right">Placeholder</li>
+                                    <li className="sidebar-item">
+                                        <input type="text" className="form-control"
+                                        value={this.props.element.placeholder}
+                                        onChange={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'placeholder')}
+                                        />
+                                    </li>
+
+                                    <li className="sidebar-title sidebar-title-right">Input name</li>
+                                    <p>This will be the name that this input will display with in the database</p>
+                                    <p><strong>names cannot be repeated</strong></p>
+                                    <li className="sidebar-item">
+                                        <input type="text" className="form-control"
+                                        value={this.props.element.name}
+                                        onChange={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'name')}
+                                        />
+                                    </li>
+
+                                    <li className="sidebar-title sidebar-title-right">Input Required</li>
+                                    <p>Indicates if this field can be let empty or not </p>
+
+                                    {
+                                        this.props.element.required ? 
+
+                                        <li className="sidebar-item">
+                                        <input type="checkbox" className="form-check-input"
+                                        onClick={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'required')}
+                                        checked
+                                        />
+                                        </li>
+
+                                        :
+
+                                        <li className="sidebar-item">
+                                        <input type="checkbox" className="form-check-input"
+                                        onClick={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'required')}
+                                        />
+                                        </li>
+
+                                    }
+
+
+                                    <li className="sidebar-title sidebar-title-right">Score Enable</li>
+                                    <p>Indicates if this field will have an score attached to the answer </p>
+
+                                    
+                                    {
+                                        this.props.element.scoreEnabled ? 
+                                        
+                                        <div>
+                                            <li className="sidebar-item">
+                                            <input type="checkbox" className="form-check-input"
+                                            onClick={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'scoreEnabled')}
+                                            checked
+                                            />
+                                            </li>
+
+                                            <li className="sidebar-title sidebar-title-right">Max Score</li>
+                                            <p>This will be the maximun score this answer can have attached to</p>
+                                            <p><strong>Min must be 1</strong></p>
+                                            <li className="sidebar-item">
+                                                <input type="number" className="form-control" min={1}
+                                                value={this.props.element.maxScore}
+                                                onChange={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'maxScore')}
+                                                />
+                                            </li>
+                                        </div>
+
+                                        :
+
+                                        <li className="sidebar-item">
+                                        <input type="checkbox" className="form-check-input"
+                                        onClick={(e) => this.props.functions.updateComponentElement(e, firstIndex, secondIndex, 'scoreEnabled')}
+                                        />
+                                        </li>
+
+                                    }
+
+
+                                    <br />
+                                    <hr />
+
+                                    <li className="sidebar-item">
+
+                                        <button className="btn btn-danger"
+                                        onClick={() => this.props.functions.deleteFormElement(firstIndex, secondIndex)}
+                                        >
+                                            Delete Element
+                                        </button>
+                                    </li>
+
+
+
+                                </div>
+                            }
+            
                         </ul>
 
                     </div>
